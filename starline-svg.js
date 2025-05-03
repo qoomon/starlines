e746e3e8e0bb8e04f0b0d237d9301e55ed0de17e
Bengt Brodersen <bengt.brodersen@gmail.com>
2025-05-03 11:28:26 +0200

    Update starline-svg.js

--- starline-svg.js
+++ starline-svg.js
@@ -31,7 +31,7 @@ export function createSvg(data) {
     let path = `M${toPathPoint(p0)} C ${toPathPoint(c1)}, ${toPathPoint(c2)}, ${toPathPoint(p1)}`
 
     let pathLength = 0;
-    if(points.length > 0) {
+    if(points.length >= 2) {
         for (let i = 1; i < points.length; i++) {
             const p0 = points[i - 1]
             const p1 = points[i]
